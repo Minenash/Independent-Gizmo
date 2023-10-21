ae9bebc6d3434085beb63a4abf5ae68ac7fd5816
Jakob <minenash@protonmail.com>
2023-10-20 20:38:55 -0400

    Updated to 1.20.2

--- src/main/java/com/minenash/independent_gizmo/mixin/InGameHudMixin.java
+++ src/main/java/com/minenash/independent_gizmo/mixin/InGameHudMixin.java
@@ -3,12 +3,10 @@ package com.minenash.independent_gizmo.mixin;
 import com.minenash.independent_gizmo.IndependentGizmo;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.DebugHud;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.option.AttackIndicator;
-import net.minecraft.client.option.GameOptions;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -32,15 +30,15 @@ public abstract class InGameHudMixin {
 		}
 	}
 
-	@Redirect(method = "renderCrosshair", at = @At(value = "FIELD", target = "Lnet/minecraft/client/option/GameOptions;debugEnabled:Z", opcode = Opcodes.GETFIELD))
-	private boolean getDebugCrosshairEnable(GameOptions options) {
+	@Redirect(method = "renderCrosshair", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/DebugHud;shouldShowDebugHud()Z"))
+	private boolean getDebugCrosshairEnable(DebugHud instance) {
 		return !renderAttackIndicator && IndependentGizmo.debugCrosshairEnable;
 	}
 
-	@Redirect(method = "renderCrosshair", at = @At(value = "INVOKE", ordinal = 0, target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIII)V"))
-	private void skipNormalCrosshairRendering(DrawContext context, Identifier texture, int x, int y, int u, int v, int width, int height) {
+	@Redirect(method = "renderCrosshair", at = @At(value = "INVOKE", ordinal = 0, target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Lnet/minecraft/util/Identifier;IIII)V"))
+	private void skipNormalCrosshairRendering(DrawContext context, Identifier texture, int x, int y, int width, int height) {
 		if (!renderAttackIndicator)
-			context.drawTexture(texture, x, y, u, v, width, height);
+			context.drawGuiTexture(texture, x, y, width, height);
 	}
 
 }
