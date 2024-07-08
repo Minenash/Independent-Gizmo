0e3480fcc750fdf6d9df2fe1d333a1a0e159960e
Jakob <minenash@protonmail.com>
2024-07-08 00:09:35 -0400

    Updated to 1.21

--- src/main/java/com/minenash/independent_gizmo/mixin/InGameHudMixin.java
+++ src/main/java/com/minenash/independent_gizmo/mixin/InGameHudMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.DebugHud;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.option.AttackIndicator;
+import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -18,15 +19,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(value = InGameHud.class, priority = 1200)
 public abstract class InGameHudMixin {
 
-	@Shadow protected abstract void renderCrosshair(DrawContext context, float tickDelta);
+	@Shadow protected abstract void renderCrosshair(DrawContext context, RenderTickCounter tickCounter);
 
 	@Unique boolean renderAttackIndicator = false;
 
 	@Inject(method = "renderCrosshair", at = @At(value = "HEAD"))
-	private void renderAttackIndicatorForDebugScreen2(DrawContext context, float tickDelta, CallbackInfo _info) {
+	private void renderAttackIndicatorForDebugScreen2(DrawContext context, RenderTickCounter tickCounter, CallbackInfo _info) {
 		if (MinecraftClient.getInstance().options.getAttackIndicator().getValue() == AttackIndicator.CROSSHAIR && !renderAttackIndicator) {
 			renderAttackIndicator = true;
-			renderCrosshair(context, tickDelta);
+			renderCrosshair(context, tickCounter);
 			renderAttackIndicator = false;
 		}
 	}
