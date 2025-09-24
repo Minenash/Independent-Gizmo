4fad7f148ce7fd0d7065a67f7e7dc520873dd120
LordChrom <sliemr137@gmail.com>
2025-09-24 17:21:40 -0500

    Added ability to resize crosshair

--- src/main/java/com/minenash/independent_gizmo/IndependentGizmo.java
+++ src/main/java/com/minenash/independent_gizmo/IndependentGizmo.java
@@ -23,24 +23,53 @@ public class IndependentGizmo implements ClientModInitializer {
 	public static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
 	public static boolean debugCrosshairEnable = false;
-	public static long lastPressed = 0;
-	public static boolean lastSavedState = false;
+	public static float debugCrosshairScale = 1f;
+
+	public static boolean stateDirty = false;
+	public static long lastSaved = 0;
+
 
 	public static final KeyBinding TOGGLE_GIZMO_CROSSHAIR = KeyBindingHelper.registerKeyBinding(
-			new KeyBinding("key.independent.gizmo.gizmo_crosshair_toggle", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_GRAVE_ACCENT, "key.categories.misc"));
+			new KeyBinding("key.independent.gizmo.gizmo_crosshair_toggle", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_GRAVE_ACCENT, "key.categories.independent_gizmo"));
+	public static final KeyBinding GIZMO_SIZE_UP = KeyBindingHelper.registerKeyBinding(
+			new KeyBinding("key.independent.gizmo.gizmo_size_up", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_KP_ADD, "key.categories.independent_gizmo"));
+	public static final KeyBinding GIZMO_SIZE_DOWN = KeyBindingHelper.registerKeyBinding(
+			new KeyBinding("key.independent.gizmo.gizmo_size_down", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_KP_SUBTRACT, "key.categories.independent_gizmo"));
 
 	@Override
 	public void onInitializeClient() {
 		read();
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
-			long time = System.currentTimeMillis();
 			while (TOGGLE_GIZMO_CROSSHAIR.wasPressed()) {
 				debugCrosshairEnable = !debugCrosshairEnable;
-				lastPressed = time;
+				stateDirty=true;
 			}
-			if (time - lastPressed > 1000 && debugCrosshairEnable != lastSavedState)
-				save();
+
+			if(GIZMO_SIZE_UP.isPressed()){
+				debugCrosshairScale *=1.015625f;
+				stateDirty=true;
+			}
+
+			if(GIZMO_SIZE_DOWN.isPressed()){
+				debugCrosshairScale *= 0.984375f;
+				stateDirty=true;
+			}
+
+			saveIfDirty();
 		});
+	}
+
+	public static void saveIfDirty(){
+		if(!stateDirty)
+			return;
+
+		long time = System.currentTimeMillis();
+
+		if(time-lastSaved < 1000)
+			return;
+
+		lastSaved=time;
+		save();
 	}
 
 	public static void save() {
@@ -50,8 +79,9 @@ public class IndependentGizmo implements ClientModInitializer {
 				Files.createFile(CONFIG);
 			JsonObject o = new JsonObject();
 			o.addProperty("debugCrosshairEnable", debugCrosshairEnable);
+			o.addProperty("debugCrosshairScale", debugCrosshairScale);
 			Files.write(CONFIG, GSON.toJson(o).getBytes());
-			lastSavedState = debugCrosshairEnable;
+			stateDirty = false;
 		}
 		catch (Exception e) {
 			LOGGER.error("[IndependentGizmo] Failed to save config");
@@ -68,7 +98,8 @@ public class IndependentGizmo implements ClientModInitializer {
 			try {
 				JsonObject o = GSON.fromJson(Files.newBufferedReader(CONFIG), JsonObject.class);
 				debugCrosshairEnable = o.get("debugCrosshairEnable").getAsBoolean();
-				lastSavedState = debugCrosshairEnable;
+				debugCrosshairScale = o.get("debugCrosshairScale").getAsFloat();
+				stateDirty = false;
 			}
 			catch (JsonSyntaxException | NullPointerException e) {
 				LOGGER.error("[CustomHud] Couldn't read the config, recreating");
