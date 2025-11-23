834053db3c2627849d75e6ccd629abdd38e008d1
LordChrom <slimer137@gmail.com>
2025-11-23 15:18:52 -0600

    1.21.10

--- src/main/java/com/minenash/independent_gizmo/IndependentGizmo.java
+++ src/main/java/com/minenash/independent_gizmo/IndependentGizmo.java
@@ -8,8 +8,12 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.debug.DebugHudEntries;
+import net.minecraft.client.gui.hud.debug.DebugHudEntryVisibility;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.lwjgl.glfw.GLFW;
@@ -28,21 +32,21 @@ public class IndependentGizmo implements ClientModInitializer {
 	public static boolean stateDirty = false;
 	public static long lastSaved = 0;
 
+	public static final KeyBinding.Category keyCategory = new KeyBinding.Category(Identifier.of("key.categories.independent_gizmo"));
 
 	public static final KeyBinding TOGGLE_GIZMO_CROSSHAIR = KeyBindingHelper.registerKeyBinding(
-			new KeyBinding("key.independent.gizmo.gizmo_crosshair_toggle", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_GRAVE_ACCENT, "key.categories.independent_gizmo"));
+			new KeyBinding("key.independent.gizmo.gizmo_crosshair_toggle", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_GRAVE_ACCENT, keyCategory));
 	public static final KeyBinding GIZMO_SIZE_UP = KeyBindingHelper.registerKeyBinding(
-			new KeyBinding("key.independent.gizmo.gizmo_size_up", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_KP_ADD, "key.categories.independent_gizmo"));
+			new KeyBinding("key.independent.gizmo.gizmo_size_up", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_KP_ADD, keyCategory));
 	public static final KeyBinding GIZMO_SIZE_DOWN = KeyBindingHelper.registerKeyBinding(
-			new KeyBinding("key.independent.gizmo.gizmo_size_down", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_KP_SUBTRACT, "key.categories.independent_gizmo"));
+			new KeyBinding("key.independent.gizmo.gizmo_size_down", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_KP_SUBTRACT, keyCategory));
 
 	@Override
 	public void onInitializeClient() {
 		read();
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
-			while (TOGGLE_GIZMO_CROSSHAIR.wasPressed()) {
-				debugCrosshairEnable = !debugCrosshairEnable;
-				stateDirty=true;
+			if(TOGGLE_GIZMO_CROSSHAIR.wasPressed()) {
+				debugCrosshairEnable= MinecraftClient.getInstance().debugHudEntryList.toggleVisibility(DebugHudEntries.THREE_DIMENSIONAL_CROSSHAIR);
 			}
 
 			if(GIZMO_SIZE_UP.isPressed()){
@@ -99,6 +103,10 @@ public class IndependentGizmo implements ClientModInitializer {
 				JsonObject o = GSON.fromJson(Files.newBufferedReader(CONFIG), JsonObject.class);
 				debugCrosshairEnable = o.get("debugCrosshairEnable").getAsBoolean();
 				debugCrosshairScale = o.get("debugCrosshairScale").getAsFloat();
+
+				DebugHudEntryVisibility visibility = debugCrosshairEnable? DebugHudEntryVisibility.ALWAYS_ON : DebugHudEntryVisibility.NEVER;
+				MinecraftClient.getInstance().debugHudEntryList.setEntryVisibility(DebugHudEntries.THREE_DIMENSIONAL_CROSSHAIR, visibility);
+
 				stateDirty = false;
 			}
 			catch (JsonSyntaxException | NullPointerException e) {
